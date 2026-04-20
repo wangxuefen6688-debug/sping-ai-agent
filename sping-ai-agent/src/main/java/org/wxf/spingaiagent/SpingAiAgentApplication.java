8ea28e8360010ac8ed56e477f4f31742430b3fc5
wangxuefen <1970611084@qq.com>
2026-04-20 15:22:36 +0800

    1

---
+++ sping-ai-agent/src/main/java/org/wxf/spingaiagent/SpingAiAgentApplication.java
@@ -0,0 +1,13 @@
+package org.wxf.spingaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingAiAgentApplication.class, args);
+    }
+
+}
