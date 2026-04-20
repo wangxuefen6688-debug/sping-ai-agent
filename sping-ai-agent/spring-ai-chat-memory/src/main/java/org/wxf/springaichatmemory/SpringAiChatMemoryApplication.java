4f5b7785a8e00f2f85eff6dba8d04036ae5978fe
wangxuefen <1970611084@qq.com>
2026-04-20 15:44:51 +0800

    1

---
+++ sping-ai-agent/spring-ai-chat-memory/src/main/java/org/wxf/springaichatmemory/SpringAiChatMemoryApplication.java
@@ -0,0 +1,13 @@
+package org.wxf.springaichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiChatMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiChatMemoryApplication.class, args);
+    }
+
+}
---
+++ sping-ai-agent/spring-ai-chat-memory/src/test/java/org/wxf/springaichatmemory/SpringAiChatMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wxf.springaichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiChatMemoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
